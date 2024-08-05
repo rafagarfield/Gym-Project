78b0e33481b953e283160e03ce34503b1395e8e4
rafagarfield <mgrafaelgarcia.2805@gmail.com>
2024-08-05 16:03:15 -0500

    add component GradientRed

---
+++ src/Components/GradientRed/GradientRed.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./GradientRed.module.css";
+
+function GradientRed() {
+  return (
+    <div className={styles.gradientContainer}>
+      
+    </div>
+  )
+}
+
+export default GradientRed
