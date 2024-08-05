61239a17c84f2f113f418fd8a162067fc86e8496
rafagarfield <mgrafaelgarcia.2805@gmail.com>
2024-08-05 12:51:22 -0500

    creacion del componente gradiente

---
+++ src/Components/Gradient/Gradient.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Gradient.module.css";
+
+function Gradient() {
+  return (
+    <div className={styles.gradientContainer}>
+      
+    </div>
+  )
+}
+
+export default Gradient
