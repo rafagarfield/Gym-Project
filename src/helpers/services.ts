5c0c2049e47dd1188f7c29aa44045516462066c9
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2024-08-11 22:28:43 -0500

    agregando helpers de servicios

---
+++ src/helpers/services.ts
@@ -0,0 +1,19 @@
+export const services = [
+    {
+        image:"https://res.cloudinary.com/df5rqpdzh/image/upload/v1723304459/barbell_fnrcx7.svg",
+        title:"Entrenamiento personalizado",
+        description:"Obtén un plan de entrenamiento a medida con el apoyo de nuestros entrenadores personales. Diseñaremos un programa adaptado a tus objetivos y necesidades específicas, asegurando que cada sesión sea efectiva y motivadora." 
+    },
+    {
+        image:"https://res.cloudinary.com/df5rqpdzh/image/upload/v1723304701/bike_mgkolr.svg",
+        title:"Spinning",
+        description:"Súbete a la bicicleta y únete a nuestras dinámicas clases de Spinning. Diseñadas para mejorar tu resistencia cardiovascular y quemar calorías, estas sesiones de alta energía te motivarán a alcanzar nuevos niveles de fitness. Ideal para todos los niveles de experiencia." 
+    },
+    {
+        image:"https://res.cloudinary.com/df5rqpdzh/image/upload/v1723304459/barbell_fnrcx7.svg",
+        title:"Aeróbicos",
+        description:"Disfruta de nuestras clases de aeróbicos llenas de energía, donde podrás mejorar tu resistencia cardiovascular, quemar calorías y tonificar tu cuerpo.  estas sesiones son perfectas para todos los niveles y te ayudarán a mantenerte en forma mientras te diviertes." 
+    },
+   
+
+]
